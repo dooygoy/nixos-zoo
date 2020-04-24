69eb47e9c8de2de7518fe0bbf55919ecaaae764c
Domagoj Miskovic <dooygoy@gmail.com>
2020-04-24 09:33:56 +0200

    configs

---
+++ config/i3/autostart.sh
@@ -0,0 +1,12 @@
+feh --bg-fill ~/Downloads/wall1.jpeg --no-xinerama &
+
+autorandr --change --default small &
+srandrd autorandr --change --default small &
+
+sleep 1
+unclutter &
+nm-applet &
+parcellite &
+pasystray &
+xautolock -locker "i3lock --color '#332233'"  -time 60 -detectsleep &
+redshift -l -36.84853:174.76349 & # auckland, nz
